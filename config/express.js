704dda1b55433028f570576702270ee6189372bc
Akshat571 <akshatdivya@infrrd.ai>
2020-11-13 14:46:02 +0530

    Enable all CORS requests

--- config/express.js
+++ config/express.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const logger = require("morgan");
 const express = require("express");
+const cors = require('cors');
 const bodyparser = require("body-parser");
 
 module.exports = function (app) {
   app.use(logger("dev"));
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(bodyparser.json());
