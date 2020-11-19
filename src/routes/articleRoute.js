08ac6102abe771b91c2abe70141a66cb0178499c
Akshat571 <akshatdivya@infrrd.ai>
2020-11-19 09:46:52 +0530

    Add feed and trending APIs

--- src/routes/articleRoute.js
+++ src/routes/articleRoute.js
@@ -2,59 +2,81 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const controller = require("../controllers/articleController");
-const { handleResponse, getPayload, verifyToken } = require("../utils");
+const { tokenAuthincator } = require("../utils");
 const StatusCodes = require('http-status-codes').StatusCodes;
 
 router.post('/createPost', function (req, res) {
     const { title, topics, content } = req.body;
     if (title != null && content != null && topics != null) {
-        const bearerHeader = req.headers['authorization'];
-        if (bearerHeader == null || bearerHeader == undefined) {
-            res.status(StatusCodes.BAD_REQUEST).json({
-                message: "BAD REQUEST"
-            })
-            return;
-        }
-        var token = getPayload(bearerHeader);
-        if (token == null) {
-            res.status(StatusCodes.BAD_REQUEST).json({
-                message: "BAD REQUEST"
-            })
-            return;
-        } else {
-            verifyToken(token, function (error, verifiedJwt) {
-                console.log("verified token-->", verifiedJwt);
-                if (error) {
-                    return res.status(StatusCodes.UNAUTHORIZED).json({
-                        message: "UNAUTHORIZED"
-                    })
-                } else {
-                    var author = verifiedJwt.email;
-                    controller.publishPost(title, topics, content, author, function (error, article) {
-                        if (error) {
-                            res.status(StatusCodes.BAD_REQUEST).json({
-                                "message": error.name
-                            })
-                            return;
-
-                        } else {
-                            return res.status(StatusCodes.OK).json({
-                                result: {
-                                    "message": "post published"
-                                }
-                            })
-                        }
-                    })
-
-                }
-            })
-        }
-
+        tokenAuthincator(req, res, function (error, verifiedJwt) {
+            if (error) {
+                return res.status(StatusCodes.UNAUTHORIZED).json({
+                    message: "UNAUTHORIZED"
+                })
+            } else {
+                var author = verifiedJwt.email;
+                controller.publishPost(title, topics, content, author, function (error, article) {
+                    if (error) {
+                        return res.status(StatusCodes.BAD_REQUEST).json({
+                            "message": error.name
+                        })
+                    } else {
+                        return res.status(StatusCodes.OK).json({
+                            result: {
+                                "message": "post published"
+                            }
+                        })
+                    }
+                })
+            }
+        })
     } else {
         res.status(StatusCodes.NO_CONTENT).json({
             "message": "NO CONTENT"
         });
     }
+})
+
+router.get("/trending", function (req, res) {
+    tokenAuthincator(req, res, function (error, verifiedJwt) {
+        if (error) {
+            return res.status(StatusCodes.UNAUTHORIZED).json({
+                message: "UNAUTHORIZED"
+            })
+        } else {
+            controller.retriveTrendingArticles(function (error, articles) {
+                if (error) {
+                    return res.json({
+                        error: {
+                            message: error.name
+                        }
+                    })
+                } else
+                    res.send(articles);
+            })
+        }
+    })
+})
+
+router.get("/feed", function (req, res) {
+    tokenAuthincator(req, res, function (error, verifiedJwt) {
+        if (error) {
+            return res.status(StatusCodes.UNAUTHORIZED).json({
+                message: "UNAUTHORIZED"
+            })
+        } else {
+            controller.retriveFeed(verifiedJwt.email, function (error, feedArr) {
+                if (error) {
+                    return res.json({
+                        error: {
+                            message: error.name
+                        }
+                    })
+                } else
+                    res.send(feedArr);
+            })
+        }
+    })
 })
 
 module.exports = router;
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-
+const StatusCodes = require('http-status-codes').StatusCodes;
 
 function handleResponse(error, result, res) {
   if (error != null) {
@@ -24,25 +24,44 @@ module.exports.tokenGenerator = function (name, email, secret, callback) {
     });
 }
 
-module.exports.verifyToken=function(token,callback){
+function verifyToken(token, callback) {
   jwt.verify(token, 'secret', (error, verifiedJwt) => {
-    if(error){
-      callback(error,null);
-    }else{
-      callback(error,verifiedJwt);
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(error, verifiedJwt);
     }
   })
 }
 
-module.exports.getPayload=function(bearerHeader){
-  if(typeof bearerHeader!==undefined){
-   const bearer=bearerHeader.split(' ');
-   const token=bearer[1];
-   return token;
+function getPayload(bearerHeader) {
+  if (typeof bearerHeader !== undefined) {
+    const bearer = bearerHeader.split(' ');
+    const token = bearer[1];
+    return token;
 
-  }else{
+  } else {
     return false;
   }
+}
+
+module.exports.tokenAuthincator = function (req, res, callback) {
+  const bearerHeader = req.headers['authorization'];
+  if (bearerHeader == null || bearerHeader == undefined) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: "BAD REQUEST"
+    })
+  }
+  var token = getPayload(bearerHeader);
+  if (token == null) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: "BAD REQUEST"
+    })
+  }
+
+  verifyToken(token, function (error, verifiedJwt) {
+    callback(error, verifiedJwt);
+  })
 }
 
 exports.handleResponse = handleResponse;
