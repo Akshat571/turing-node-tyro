7cf873bb9b10b6cd99c47e7017ff92af04efb15f
Akshat571 <akshatdivya@infrrd.ai>
2020-11-19 18:14:01 +0530

    Code Refactoring

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -1,14 +1,14 @@
-const TopicDao=require("../dao/topicDao");
+const TopicDao = require("../dao/topicDao");
 
-module.exports.getSimilarTopics=function(topic,callback){
-    TopicDao.findSimilarTopics(topic,function(error,result){
-        callback(error,result);
+module.exports.getSimilarTopics = function (topic, callback) {
+    TopicDao.findSimilarTopics(topic, function (error, result) {
+        callback(error, result);
     })
 
 }
 
-module.exports.followTopic=function(topicId,userEmail,callback){
-    TopicDao.addTopicToPerson(topicId,userEmail,function(error,result){
-        callback(error,result)
+module.exports.followTopic = function (topicId, userEmail, callback) {
+    TopicDao.addTopicToPerson(topicId, userEmail, function (error, result) {
+        callback(error, result)
     })
 }
