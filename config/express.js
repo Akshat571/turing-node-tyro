18452ce9ed2a88841371f356fd7bf10cd007062b
Akshat571 <akshatdivya@infrrd.ai>
2020-11-22 03:43:55 +0530

    refactor code

--- config/express.js
+++ config/express.js
@@ -7,22 +7,6 @@ const bodyparser = require("body-parser");
 const swaggerUi = require('swagger-ui-express');
 swaggerDocument = require('../swagger.json');
 
-// const swaggerOptions = {
-//   definition: {
-//     info: {
-//       title: 'Project Tyro REST endpoints',
-//       description: ' API Thedocumentation contains all the REST endpoints related to project Tyro.',
-//       version: '1.0.0',
-//       contact: {
-//         name: "API support",
-//         email: "akshatdivya@infrrd.ai, akshathajanardhan@infrrd.ai"
-//       },
-//       basePath: '/',
-//       servers: ["http://localhost:3000"]
-//     }
-//   },
-//   apis: ['E:/turing-node-tyro/src/routes/userRoute.js']
-// }
 module.exports = function (app) {
   app.use(logger("dev"));
   app.use(cors());
